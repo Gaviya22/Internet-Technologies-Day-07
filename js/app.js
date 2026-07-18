6991798c1952c60d3162635483317f27971c7abd
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-18 12:45:47 +0530

    Refactor app.js: Comment out unused customerList code and retain inStockItems logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
-const customerList = [];
+// const customerList = [];
 
-customerList.push(1);
-customerList.push(2);
-customerList.push(3);
+// customerList.push(1);
+// customerList.push(2);
+// customerList.push(3);
 
-let revAr = customerList.reverse();
+// let revAr = customerList.reverse();
 
-console.log(revAr);
+// console.log(revAr);
+
+// let inStockItems = products.filter(
+//     function (product) {
+//         return productFilter(product)
+//     }
+// )
+
+let inStockItems = product.filter(product => product.inStock == false);
+
+function getSum(num1, num2){
+    return num1+num2;
+}
+
+getSum(10,20);
+
+let getSum = function (num1,num2){
+    return num1+num2;
+}
+
+console.log(getSum(10,20));
+
+let getSum = (num1, num2) => {
+    return num1+num2;
+}
+
+console.log(getSum(10,20));
+
+let sample = txtValue => {
+    return txtValue;
+}
+
+console.log(sample("Hi saman"));
+
+let getSum = (num1, num2) => num1+num2;
+
+console.log(getSum(10,20));
+
+let sample = txtValue => txtValue;
+
+console.log(sample("Hi saman"));
