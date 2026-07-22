9457e298a183ee3eb4b343655b0122a436f44fb3
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-22 05:43:48 +0530

    New Commit

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ fetch("https://fakestoreapi.com/products").then(res=>res.json()).then(data=>{
                 <div class="col-md-4 col-lg-3 mb-4">
                     <div class="card h-100 shadow">
 
-                        <img src="${product.ima}"
+                        <img src="${product.imag}"
                              class="card-img-top p-3"
                              style="height:250px; object-fit:contain;"
                              alt="${product.title}">
